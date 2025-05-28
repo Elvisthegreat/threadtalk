3e9c517428ff9b9fc81f174e3cad79426249bd48
Elvisthegreat <amadinelvis6@gmail.com>
2025-05-28 17:05:21 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+#[Route('/', name: 'home')]
+
+class HomeController extends AbstractController{
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
