4ce8807262695552e590bd2ae7a6ab8fb5b9ace3
Elvisthegreat <amadinelvis6@gmail.com>
2025-05-28 17:46:04 +0200

    Updated home controller and templates

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,13 +2,15 @@
 
 namespace App\Controller;
 
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-#[Route('/', name: 'home')]
 
-class HomeController extends AbstractController{
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig');
